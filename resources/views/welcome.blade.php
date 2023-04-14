e7f0796427c111233916a8f00480f068d79dbb62
RickyDj <rickydjunaidy@ymail.com>
2023-04-14 23:25:37 +0700

    feat: image and behold! the intro text!

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <title> API </title>
+        <title> LARAVEL RESTFUL API </title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     </head>
     <body>
-        <div style="display: flex; justify-content: center; align-items: center; height: 100vh; margin: 0;">
-            
-            <div class="col-xs-12"><img src="https://ichef.bbci.co.uk/news/976/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg.webp" style="text-align:center; width:200px; height: 200px;" /></div>
-            <div class="col-xs-12">Ricky Djunaidy API</div>
-        </div>
+    <div class="container-fluid d-flex justify-content-center align-items-center" style="height:100vh">
+            <div>
+                <img draggable="false" style="text-align:center; width:200px; height: 200px;" src="https://ichef.bbci.co.uk/news/976/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg.webp" />
+                <div style="text-align:center;">Ricky Djunaidy API</div>
+            </div>
+    </div>
+
     </body>
 </html>
