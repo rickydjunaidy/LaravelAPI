1b1f773ef8984d3da95beffa30daa0d64697a3f4
RickyDj <rickydjunaidy@ymail.com>
2023-04-14 18:29:28 +0700

    First Init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <title> API </title>
+    </head>
+    <body>
+        <div style="display: flex; justify-content: center; align-items: center; height: 100vh; margin: 0;">
+            
+            <div class="col-xs-12"><img src="https://ichef.bbci.co.uk/news/976/cpsprodpb/16620/production/_91408619_55df76d5-2245-41c1-8031-07a4da3f313f.jpg.webp" style="text-align:center; width:200px; height: 200px;" /></div>
+            <div class="col-xs-12">Ricky Djunaidy API</div>
+        </div>
+    </body>
+</html>
